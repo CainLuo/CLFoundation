944fcb9b00d04986a37c410653e783ee324f7e9b
CainLuo <350116542@qq.com>
2018-01-29 22:43:52 +0800

    更新文件

--- CLFoundation/CLFoundation.h
+++ CLFoundation/CLFoundation.h
@@ -13,44 +13,34 @@
 //  Copyright © 2016年 Cain. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
-//! Project version number for CLFoundation.
-FOUNDATION_EXPORT double CLFoundationVersionNumber;
-
-//! Project version string for CLFoundation.
-FOUNDATION_EXPORT const unsigned char CLFoundationVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <CLFoundation/PublicHeader.h>
-
 #pragma mark - NSString+CLRegular
-#import <CLFoundation/NSString+CLRegular.h>
-#import <CLFoundation/NSString+CLString.h>
-#import <CLFoundation/NSString+CLEncryption.h>
+#import "NSString+CLRegular.h"
+#import "NSString+CLString.h"
+#import "NSString+CLEncryption.h"
 
 #pragma mark - NSBundle+CLBundle
-#import <CLFoundation/NSBundle+CLBundle.h>
+#import "NSBundle+CLBundle.h"
 
 #pragma mark - NSDate+CLDate
-#import <CLFoundation/NSDate+CLDate.h>
+#import "NSDate+CLDate.h"
 
 #pragma mark - NSData+CLData
-#import <CLFoundation/NSData+CLData.h>
+#import "NSData+CLData.h"
 
 #pragma mark - NSURL+CLURL
-#import <CLFoundation/NSURL+CLURL.h>
+#import "NSURL+CLURL.h"
 
 #pragma mark - CLDefine
-#import <CLFoundation/CLDefine.h>
+#import "CLDefine.h"
 
 #pragma mark - CLAttributedString
-#import <CLFoundation/NSAttributedString+CLAttributedString.h>
-#import <CLFoundation/NSMutableAttributedString+CLMutableAttributedString.h>
+#import "NSAttributedString+CLAttributedString.h"
+#import "NSMutableAttributedString+CLMutableAttributedString.h"
 
 #pragma mark - CLArray
-#import <CLFoundation/NSArray+CLArray.h>
-#import <CLFoundation/NSMutableArray+CLMutableArray.h>
+#import "NSArray+CLArray.h"
+#import "NSMutableArray+CLMutableArray.h"
 
 #pragma mark - CLDictionary
-#import <CLFoundation/NSDictionary+CLDictionary.h>
-#import <CLFoundation/NSMutableDictionary+CLMutableDictionary.h>
+#import "NSDictionary+CLDictionary.h"
+#import "NSMutableDictionary+CLMutableDictionary.h"
