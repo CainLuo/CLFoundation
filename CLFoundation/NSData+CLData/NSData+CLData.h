1698c904c31d91bd94db18e50cca37ab0a789a43
CainLuo <350116542@qq.com>
2018-02-28 23:14:28 +0800

    更新NSData+CLData

--- CLFoundation/NSData+CLData/NSData+CLData.h
+++ CLFoundation/NSData+CLData/NSData+CLData.h
@@ -9,6 +9,12 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSInteger, CLDataBaseType) {
+    
+    CLDataBaseType64 = 64,
+    CLDataBaseType76 = 76
+};
+
 @interface NSData (CLData)
 
 /**
@@ -20,5 +26,74 @@
  */
 + (NSData *)cl_compressOriginalImage:(UIImage *)image
                   compressionQuality:(CGFloat)compressionQuality;
+
+/**
+ 将制定的APNs NSData转成NSString
+
+ @param data NSData
+ @return NSString
+ */
++ (NSString *)cl_replacingAPNsTokenWithData:(NSData *)data;
+
+#pragma mark - Base 64加密/解密
+/**
+ 将Base 64的字符串转成NSData
+
+ @param string NSString
+ @return NSData
+ */
++ (NSData *)cl_transformDataWithBase64EncodedString:(NSString *)string;
+
+/**
+ 将指定的NSData转成64或76的字符串
+
+ @param data NSData
+ @param wrapWidth CLDataBaseType
+ @return NSString
+ */
++ (NSString *)cl_transformBase64EncodedStringWithData:(NSData *)data
+                                            wrapWidth:(CLDataBaseType)wrapWidth;
+
+#pragma mark - AES加密/解密
+/**
+ 利用AES加密NSData
+
+ @param key NSString
+ @param encryptData NSData
+ @return NSData
+ */
+- (NSData *)cl_encryptedDataWithAESKey:(NSString *)key
+                           encryptData:(NSData *)encryptData;
+
+/**
+ 利用AES解密NSData
+
+ @param key NSString
+ @param decryptData NSData
+ @return NSData
+ */
+- (NSData *)cl_decryptedDataWithAESKey:(NSString *)key
+                           decryptData:(NSData *)decryptData;
+
+#pragma mark - 3DES加密/解密
+/**
+ 利用3DES加密NSData
+ 
+ @param key NSString
+ @param encryptData NSData
+ @return NSData
+ */
+- (NSData *)cl_encryptedDataWith3DESKey:(NSString *)key
+                            encryptData:(NSData *)encryptData;
+
+/**
+ 利用3DES解密NSData
+ 
+ @param key NSString
+ @param decryptData NSData
+ @return NSData
+ */
+- (NSData *)cl_decryptedDataWith3DEKey:(NSString *)key
+                           decryptData:(NSData *)decryptData;
 
 @end
--- CLFoundation/NSData+CLData/NSData+CLData.m
+++ CLFoundation/NSData+CLData/NSData+CLData.m
@@ -7,6 +7,8 @@
 //
 
 #import "NSData+CLData.h"
+#import "NSString+CLString.h"
+#import <CommonCrypto/CommonCryptor.h>
 
 @implementation NSData (CLData)
 
@@ -38,6 +40,204 @@
     }
     
     return cl_data;
+}
+
++ (NSString *)cl_replacingAPNsTokenWithData:(NSData *)data {
+    
+    NSString *cl_replacingStringOne   = [[data description] stringByReplacingOccurrencesOfString:@"<" withString: @""];
+    NSString *cl_replacingStringTwo   = [cl_replacingStringOne stringByReplacingOccurrencesOfString: @">" withString: @""];
+    NSString *cl_replacingStringThree = [cl_replacingStringTwo stringByReplacingOccurrencesOfString: @" " withString: @""];
+
+    return cl_replacingStringThree;
+}
+
+#pragma mark - Base 64
++ (NSData *)cl_transformDataWithBase64EncodedString:(NSString *)string {
+    
+    if ([NSString cl_checkEmptyWithString:string]) {
+        
+        return nil;
+    }
+    
+    NSData *cl_decodedData = [[NSData alloc] initWithBase64EncodedString:string
+                                                                 options:NSDataBase64DecodingIgnoreUnknownCharacters];
+
+    return [cl_decodedData length] ? cl_decodedData: nil;
+}
+
++ (NSString *)cl_transformBase64EncodedStringWithData:(NSData *)data
+                                            wrapWidth:(CLDataBaseType)wrapWidth {
+    
+    if (![data length]) {
+        
+        return nil;
+    }
+    
+    NSString *cl_dataEncodedString = nil;
+    
+    switch (wrapWidth) {
+        case CLDataBaseType64: {
+            
+            return [data base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+        } case CLDataBaseType76: {
+            
+            return [data base64EncodedStringWithOptions:NSDataBase64Encoding76CharacterLineLength];
+            
+        } default: {
+            
+            cl_dataEncodedString = [data base64EncodedStringWithOptions:(NSDataBase64EncodingOptions)0];
+        }
+    }
+
+    if (!wrapWidth || wrapWidth >= [cl_dataEncodedString length]) {
+        
+        return cl_dataEncodedString;
+    }
+    
+    wrapWidth = (wrapWidth / 4) * 4;
+    
+    NSMutableString *cl_resultString = [NSMutableString string];
+    
+    for (NSUInteger i = 0; i < [cl_dataEncodedString length]; i+= wrapWidth) {
+        
+        if (i + wrapWidth >= [cl_dataEncodedString length]) {
+            
+            [cl_resultString appendString:[cl_dataEncodedString substringFromIndex:i]];
+            
+            break;
+        }
+        
+        [cl_resultString appendString:[cl_dataEncodedString
+                                       substringWithRange:NSMakeRange(i, wrapWidth)]];
+        
+        [cl_resultString appendString:@"\r\n"];
+    }
+    
+    return cl_resultString;
+}
+
+#pragma mark - AES
+- (NSData *)cl_encryptedDataWithAESKey:(NSString *)key
+                           encryptData:(NSData *)encryptData {
+    
+    NSData *cl_keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
+    
+    size_t cl_dataMoved;
+    NSMutableData *cl_encryptedData = [NSMutableData dataWithLength:self.length + kCCBlockSizeAES128];
+    
+    CCCryptorStatus cl_cryptorStatus = CCCrypt(kCCEncrypt,
+                                               kCCAlgorithmAES128,
+                                               kCCOptionPKCS7Padding,
+                                               cl_keyData.bytes,
+                                               cl_keyData.length,
+                                               encryptData.bytes,
+                                               self.bytes,
+                                               self.length,
+                                               cl_encryptedData.mutableBytes,
+                                               cl_encryptedData.length,
+                                               &cl_dataMoved);
+    
+    if (cl_cryptorStatus == kCCSuccess) {
+        
+        cl_encryptedData.length = cl_dataMoved;
+        
+        return cl_encryptedData;
+    }
+    
+    return nil;
+}
+
+- (NSData *)cl_decryptedDataWithAESKey:(NSString *)key
+                           decryptData:(NSData *)decryptData {
+    
+    NSData *cl_keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
+    
+    size_t cl_dataMoved;
+    
+    NSMutableData *cl_decryptedData = [NSMutableData dataWithLength:self.length + kCCBlockSizeAES128];
+    
+    CCCryptorStatus cl_cryptorStatus = CCCrypt(kCCDecrypt,
+                                               kCCAlgorithmAES128,
+                                               kCCOptionPKCS7Padding,
+                                               cl_keyData.bytes,
+                                               cl_keyData.length,
+                                               decryptData.bytes,
+                                               self.bytes,
+                                               self.length,
+                                               cl_decryptedData.mutableBytes,
+                                               cl_decryptedData.length,
+                                               &cl_dataMoved);
+    
+    if (cl_cryptorStatus == kCCSuccess) {
+        
+        cl_decryptedData.length = cl_dataMoved;
+        
+        return cl_decryptedData;
+    }
+    
+    return nil;
+}
+
+#pragma mark - 3DES
+- (NSData *)cl_encryptedDataWith3DESKey:(NSString *)key
+                            encryptData:(NSData *)encryptData {
+    
+    NSData *cl_keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
+    
+    size_t cl_dataMoved;
+    NSMutableData *cl_encryptedData = [NSMutableData dataWithLength:self.length + kCCBlockSize3DES];
+    
+    CCCryptorStatus cl_cryptorStatus = CCCrypt(kCCEncrypt,
+                                               kCCAlgorithm3DES,
+                                               kCCOptionPKCS7Padding,
+                                               cl_keyData.bytes,
+                                               cl_keyData.length,
+                                               encryptData.bytes,
+                                               self.bytes,
+                                               self.length,
+                                               cl_encryptedData.mutableBytes,
+                                               cl_encryptedData.length,
+                                               &cl_dataMoved);
+    
+    if (cl_cryptorStatus == kCCSuccess) {
+        
+        cl_encryptedData.length = cl_dataMoved;
+        
+        return cl_encryptedData;
+    }
+    
+    return nil;
+}
+
+- (NSData *)cl_decryptedDataWith3DEKey:(NSString *)key
+                           decryptData:(NSData *)decryptData {
+    
+    NSData *cl_keyData = [key dataUsingEncoding:NSUTF8StringEncoding];
+    
+    size_t cl_dataMoved;
+    
+    NSMutableData *cl_decryptedData = [NSMutableData dataWithLength:self.length + kCCBlockSize3DES];
+    
+    CCCryptorStatus cl_cryptorStatus = CCCrypt(kCCDecrypt,
+                                               kCCAlgorithm3DES,
+                                               kCCOptionPKCS7Padding,
+                                               cl_keyData.bytes,
+                                               cl_keyData.length,
+                                               decryptData.bytes,
+                                               self.bytes,
+                                               self.length,
+                                               cl_decryptedData.mutableBytes,
+                                               cl_decryptedData.length,
+                                               &cl_dataMoved);
+    
+    if (cl_cryptorStatus == kCCSuccess) {
+        
+        cl_decryptedData.length = cl_dataMoved;
+        
+        return cl_decryptedData;
+    }
+    
+    return nil;
 }
 
 @end
