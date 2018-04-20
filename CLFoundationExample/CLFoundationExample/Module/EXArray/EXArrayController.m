732e3311204aedb36830efe034ec0f401cce2b16
CainLuo <350116542@qq.com>
2018-04-20 14:28:29 +0800

    添加Array的API使用示例

--- CLFoundationExample/CLFoundationExample/Module/EXArray/EXArrayController.m
+++ CLFoundationExample/CLFoundationExample/Module/EXArray/EXArrayController.m
@@ -17,56 +17,71 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self cl_array];
-    [self cl_mutableArray];
+    [self ex_array];
+    [self ex_mutableArray];
     
     [self ex_reloadTextView];
 }
 
-- (void)cl_array {
+- (void)ex_array {
     
     //    这么写会直接Cash
     //    NSArray *cl_array = [NSArray arrayWithObject:nil];
     
-    NSArray *cl_safeArray = [NSArray cl_initSafeArrayWithObject:nil];
+    NSArray *ex_safeArray = [NSArray cl_initSafeArrayWithObject:nil];
     
     [self.ex_textViewString appendString:@"----------NSArray----------\n"];
-    [self.ex_textViewString appendFormat:@"创建数组: %@\n", cl_safeArray];
-    [self.ex_textViewString appendFormat:@"获取对象的索引: %lud\n", (unsigned long)[cl_safeArray cl_safeIndexOfObject:@"Hello Word"]];
-    [self.ex_textViewString appendFormat:@"获取指定区间的数组: %@\n", [cl_safeArray cl_safeArrayWithRange:NSMakeRange(0, 10)]];
-    [self.ex_textViewString appendFormat:@"获取指定索引的对象: %@\n", [cl_safeArray cl_safeObjectAtIndex:10]];
+    [self.ex_textViewString appendFormat:@"创建数组: %@\n", ex_safeArray];
+    [self.ex_textViewString appendFormat:@"获取对象的索引: %lud\n", (unsigned long)[ex_safeArray cl_safeIndexOfObject:@"Hello Word"]];
+    [self.ex_textViewString appendFormat:@"获取指定区间的数组: %@\n", [ex_safeArray cl_safeArrayWithRange:NSMakeRange(0, 10)]];
+    [self.ex_textViewString appendFormat:@"获取指定索引的对象: %@\n", [ex_safeArray cl_safeObjectAtIndex:10]];
     [self.ex_textViewString appendFormat:@"获取指定Plist文件的数组: %@\n\n", [NSArray cl_getArrayWithPlistName:@"Array"]];
+    
+    NSArray *ex_friendArray = @[@"A", @"B", @"C", @"E", @"F"];
+    
+    NSData *ex_dictionaryData = [NSPropertyListSerialization dataWithPropertyList:ex_friendArray
+                                                                           format:NSPropertyListBinaryFormat_v1_0
+                                                                          options:NSPropertyListImmutable
+                                                                            error:nil];
+    
+    NSArray *ex_array = [NSArray cl_arrayWithPlistData:ex_dictionaryData];
+    
+    NSMutableArray *ex_mutableArray = [[NSMutableArray alloc] initWithArray:ex_array];
+    
+    [self.ex_textViewString appendFormat:@"获取NSData序列化数组: %@\n\n", ex_array];
+    [self.ex_textViewString appendFormat:@"获取倒序排列的数组: %@\n\n", [ex_mutableArray cl_getReverseArray]];
+    [self.ex_textViewString appendFormat:@"获取乱序排列的数组: %@\n\n", [ex_mutableArray cl_getDisorderArray]];
 }
 
-- (void)cl_mutableArray {
+- (void)ex_mutableArray {
     
-    NSMutableArray *cl_safeMutableArray = [[NSMutableArray alloc] init];
+    NSMutableArray *ex_safeMutableArray = [[NSMutableArray alloc] init];
     
-    [cl_safeMutableArray cl_addSafeObject:nil];
-    [cl_safeMutableArray cl_addSafeObject:@"Hello Word"];
+    [ex_safeMutableArray cl_addSafeObject:nil];
+    [ex_safeMutableArray cl_addSafeObject:@"Hello Word"];
     
     [self.ex_textViewString appendString:@"----------NSMutableArray----------\n"];
-    [self.ex_textViewString appendFormat:@"安全的添加一个对象: %@\n", cl_safeMutableArray];
+    [self.ex_textViewString appendFormat:@"安全的添加一个对象: %@\n", ex_safeMutableArray];
     
-    [cl_safeMutableArray cl_insertSafeObject:@"13800138000"
+    [ex_safeMutableArray cl_insertSafeObject:@"13800138000"
                                        index:10];
     
-    [self.ex_textViewString appendFormat:@"安全的插入一个对象: %@\n", cl_safeMutableArray];
+    [self.ex_textViewString appendFormat:@"安全的插入一个对象: %@\n", ex_safeMutableArray];
     
-    NSIndexSet *cl_indexSet = [NSIndexSet indexSetWithIndex:1000];
+    NSIndexSet *ex_indexSet = [NSIndexSet indexSetWithIndex:1000];
     
-    [cl_safeMutableArray cl_insertSafeArray:@[@"acount", @"passowrd"]
-                                   indexSet:cl_indexSet];
+    [ex_safeMutableArray cl_insertSafeArray:@[@"acount", @"passowrd"]
+                                   indexSet:ex_indexSet];
     
-    [self.ex_textViewString appendFormat:@"安全的插入一个数组: %@\n", cl_safeMutableArray];
+    [self.ex_textViewString appendFormat:@"安全的插入一个数组: %@\n", ex_safeMutableArray];
     
-    [cl_safeMutableArray cl_safeRemoveObjectAtIndex:10];
+    [ex_safeMutableArray cl_safeRemoveObjectAtIndex:10];
     
-    [self.ex_textViewString appendFormat:@"安全的删除索引为10的对象: %@\n", cl_safeMutableArray];
+    [self.ex_textViewString appendFormat:@"安全的删除索引为10的对象: %@\n", ex_safeMutableArray];
     
-    [cl_safeMutableArray cl_safeRemoveObjectsInRange:NSMakeRange(2, 1)];
+    [ex_safeMutableArray cl_safeRemoveObjectsInRange:NSMakeRange(2, 1)];
     
-    [self.ex_textViewString appendFormat:@"安全的区域为2~1的对象: %@\n", cl_safeMutableArray];
+    [self.ex_textViewString appendFormat:@"安全的区域为2~1的对象: %@\n", ex_safeMutableArray];
 }
 
 @end
