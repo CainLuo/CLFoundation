e247b67b2a02220560c34c8f3ada85f8b1ef7e32
CainLuo <350116542@qq.com>
2018-04-20 15:41:53 +0800

    添加NSData+CLData的使用示例

--- CLFoundationExample/CLFoundationExample/Module/EXData/EXDataController.m
+++ CLFoundationExample/CLFoundationExample/Module/EXData/EXDataController.m
@@ -17,18 +17,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    UIImage *cl_image = [UIImage imageNamed:@"1"];
+    UIImage *ex_image = [UIImage imageNamed:@"1"];
     
     // 压缩前
-    NSData *cl_imageData = UIImageJPEGRepresentation(cl_image, 1.0);
+    NSData *ex_imageData = UIImageJPEGRepresentation(ex_image, 1.0);
 
     // 压缩后
-    NSData *cl_tranformImageData = [NSData cl_compressOriginalImage:cl_image
+    NSData *ex_tranformImageData = [NSData cl_compressOriginalImage:ex_image
                                                  compressionQuality:0.5];
     
-    [self.ex_textViewString appendFormat:@"压缩前: %lud\n", cl_imageData.length];
-    [self.ex_textViewString appendFormat:@"压缩后: %lud", cl_tranformImageData.length];
+    NSData *ex_md2Data = [ex_tranformImageData cl_encryptredMD2Data];
+    NSData *ex_md4Data = [ex_tranformImageData cl_encryptredMD4Data];
+    NSData *ex_md5Data = [ex_tranformImageData cl_encryptredMD5Data];
     
+    NSData *ex_sha1Data   = [ex_tranformImageData cl_encryptredSHA1Data];
+    NSData *ex_sha224Data = [ex_tranformImageData cl_encryptredSHA224Data];
+    NSData *ex_sha255Data = [ex_tranformImageData cl_encryptredSHA256Data];
+    NSData *ex_sha384Data = [ex_tranformImageData cl_encryptredSHA384Data];
+
+    [self.ex_textViewString appendString:@"压缩相关:\n"];
+    [self.ex_textViewString appendFormat:@"压缩前: %lud\n", ex_imageData.length];
+    [self.ex_textViewString appendFormat:@"压缩后: %lud\n", ex_tranformImageData.length];
+
+    [self.ex_textViewString appendString:@"\n加密相关:\n"];
+    [self.ex_textViewString appendFormat:@"获取MD2加密后的NSData: %lud\n", ex_md2Data.length];
+    [self.ex_textViewString appendFormat:@"获取MD4加密后的NSData: %lud\n", ex_md4Data.length];
+    [self.ex_textViewString appendFormat:@"获取MD5加密后的NSData: %lud\n", ex_md5Data.length];
+    [self.ex_textViewString appendFormat:@"获取SHA1加密后的NSData: %lud\n", ex_sha1Data.length];
+    [self.ex_textViewString appendFormat:@"获取SHA224加密后的NSData: %lud\n", ex_sha224Data.length];
+    [self.ex_textViewString appendFormat:@"获取SHA256加密后的NSData: %lud\n", ex_sha255Data.length];
+    [self.ex_textViewString appendFormat:@"获取SHA384加密后的NSData: %lud\n", ex_sha384Data.length];
+
     [self ex_reloadTextView];
 }
 
