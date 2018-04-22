0657d54ff80a9906df1a37418e4481376481045f
CainLuo <350116542@qq.com>
2018-04-23 01:00:20 +0800

    添加使用示例

--- CLFoundationExample/CLFoundationExample/Module/EXDictionary/EXDictionaryController.m
+++ CLFoundationExample/CLFoundationExample/Module/EXDictionary/EXDictionaryController.m
@@ -30,12 +30,29 @@
     [self.ex_textViewString appendString:@"----------NSDictionary----------\n"];
     [self.ex_textViewString appendFormat:@"URL Dictionary: %@\n\n", cl_urlDictionary];
     
-    [self.ex_textViewString appendString:@"----------NSMutableDictionary----------\n"];
+    [self.ex_textViewString appendString:@"\n----------NSMutableDictionary----------\n"];
     [self.ex_textViewString appendFormat:@"安全的存储一个空的键值对: %@\n", cl_mutableDictionary];
     [self.ex_textViewString appendFormat:@"安全的获取一个空的Key: %@\n", [cl_mutableDictionary cl_safeKeyForValue:@"13800138000"]];
     [self.ex_textViewString appendFormat:@"安全的获取一个空的Value: %@\n", [cl_mutableDictionary cl_safeObjectForKey:@"acount"]];
+    
+    NSDictionary *ex_dictionaryInfo = @{@"name":@"xiaoming",
+                                        @"age":@"19"};
+    
+    NSData *ex_dictionaryData = [NSPropertyListSerialization dataWithPropertyList:ex_dictionaryInfo
+                                                                           format:NSPropertyListBinaryFormat_v1_0
+                                                                          options:NSPropertyListImmutable
+                                                                            error:nil];
+    
+    NSDictionary *ex_dictionary = [NSDictionary cl_dictionaryWithPlistData:ex_dictionaryData];
 
-    [self ex_reloadTextView];    
+    [self.ex_textViewString appendString:@"\n----------序列化NSDictionary----------\n"];
+    [self.ex_textViewString appendFormat:@"获取长度为\"%ld\"的NSData序列化字典: %@\n\n", ex_dictionaryData.length, ex_dictionary];
+    [self.ex_textViewString appendFormat:@"获取的\"%@\"排序好的Keys: %@\n\n", ex_dictionary, [ex_dictionary cl_getAllKeysSorted]];
+    [self.ex_textViewString appendFormat:@"获取的\"%@\"排序好的Values: %@\n\n", ex_dictionary, [ex_dictionary cl_getAllValuesSortedByKeys]];
+    [self.ex_textViewString appendFormat:@"查看\"%@\"是否包含\"name\": %@\n\n", ex_dictionary, [ex_dictionary cl_containsObjectForKey:@"name"] ? @"YES" :@"NO"];
+    [self.ex_textViewString appendFormat:@"获取\"%@\"指定Keys的NSDictionary: %@\n\n", ex_dictionary, [ex_dictionary cl_getDictionaryForKeys:@[@"age"]]];
+    
+    [self ex_reloadTextView];
 }
 
 @end
