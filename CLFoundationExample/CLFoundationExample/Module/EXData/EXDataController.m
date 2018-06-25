a37bd28bb6cf4007ba172d075ddae928579f66a1
CainLuo <350116542@qq.com>
2018-06-25 12:43:58 +0800

    添加使用示例

--- CLFoundationExample/CLFoundationExample/Module/EXData/EXDataController.m
+++ CLFoundationExample/CLFoundationExample/Module/EXData/EXDataController.m
@@ -36,7 +36,7 @@
     NSData *ex_sha384Data = [ex_tranformImageData cl_encryptredSHA384Data];
     NSData *ex_sha512Data = [ex_tranformImageData cl_encryptredSHA512Data];
 
-    NSString *cl_encodingKey  = @"123";
+    NSString *cl_encodingKey  = @"1234567890123456";
 
     [self.ex_textViewString appendString:@"压缩相关:\n"];
     [self.ex_textViewString appendFormat:@"压缩前: %lud\n", ex_imageData.length];
@@ -58,8 +58,89 @@
     [self.ex_textViewString appendFormat:@"获取使用HASH Key:\"%@\" SHA256加密后的NSData: %@\n", cl_encodingKey, [ex_tranformImageData cl_hmacEncryptredSHA256StringWithKey:cl_encodingKey]];
     [self.ex_textViewString appendFormat:@"获取使用HASH Key:\"%@\" SHA384加密后的NSData: %@\n", cl_encodingKey, [ex_tranformImageData cl_hmacEncryptredSHA384StringWithKey:cl_encodingKey]];
     [self.ex_textViewString appendFormat:@"获取使用HASH Key:\"%@\" SHA512加密后的NSData: %@\n", cl_encodingKey, [ex_tranformImageData cl_hmacEncryptredSHA512StringWithKey:cl_encodingKey]];
-    
+
+//    [self ex_dataWithAES128];
+    [self ex_dataWith3DES];
+
     [self ex_reloadTextView];
+}
+
+- (void)ex_dataWithAES128 {
+    
+    UIImage *ex_image = [UIImage imageNamed:@"1"];
+
+    NSString *cl_encodingKey  = @"1234567890123456";
+
+    NSData *ex_tranformImageData = [NSData cl_compressOriginalImage:ex_image
+                                                 compressionQuality:0.5];
+
+    [self.ex_textViewString appendString:@"\nAES 128加密相关:\n"];
+    
+    NSData *ex_aes128EncryptData = [ex_tranformImageData cl_encryptedDataWithAESKey:cl_encodingKey
+                                                                        encryptData:ex_tranformImageData];
+    NSData *ex_aes128ECBEncryptData = [ex_tranformImageData cl_encryptedECBDataWithAESKey:cl_encodingKey
+                                                                              encryptData:ex_tranformImageData];
+    
+    [self.ex_textViewString appendFormat:@"获取使用AES Key:\"%@\" AES128 CBC加密后的NSData: %lud\n", cl_encodingKey, ex_aes128EncryptData.length];
+    [self.ex_textViewString appendFormat:@"获取使用AES Key:\"%@\" AES128 ECB加密后的NSData: %lud\n", cl_encodingKey, ex_aes128EncryptData.length];
+
+    NSData *ex_aes128DecryptData = [ex_aes128EncryptData cl_decryptedDataWithAESKey:cl_encodingKey
+                                                                        decryptData:ex_tranformImageData];
+    NSData *ex_aes128ECBDecryptData = [ex_aes128ECBEncryptData cl_decryptedECBDataWithAESKey:cl_encodingKey
+                                                                                 decryptData:ex_tranformImageData];
+    
+    
+    NSLog(@"解密后AES 128 CBC长度: %ld", ex_aes128DecryptData.length);
+    NSLog(@"解密后AES 128 ECB长度: %ld", ex_aes128ECBDecryptData.length);
+    
+    UIImage *ex_decryptImage = [UIImage imageWithData:ex_aes128ECBDecryptData];
+    
+    UIImageView *ex_imageView = [[UIImageView alloc] initWithImage:ex_decryptImage];
+    
+    ex_imageView.frame = self.view.bounds;
+    
+    [self.view addSubview:ex_imageView];
+}
+
+- (void)ex_dataWith3DES {
+    
+    UIImage *ex_image = [UIImage imageNamed:@"1"];
+    
+    NSString *cl_encodingKey  = @"12345678";
+    
+    NSData *ex_tranformImageData = [NSData cl_compressOriginalImage:ex_image
+                                                 compressionQuality:0.5];
+
+    [self.ex_textViewString appendString:@"\n3DES加密相关:\n"];
+    
+    NSLog(@"加密前的Data长度: %ld", ex_tranformImageData.length);
+    
+    NSData *ex_3desEncryptData = [ex_tranformImageData cl_encryptedDataWith3DESKey:cl_encodingKey
+                                                                       encryptData:ex_tranformImageData];
+    NSData *ex_3desECBEncryptData = [ex_tranformImageData cl_encryptedECBDataWithAESKey:cl_encodingKey
+                                                                            encryptData:ex_tranformImageData];
+
+    
+    NSLog(@"加密后的3DES CBC长度: %ld", ex_3desEncryptData.length);
+
+    [self.ex_textViewString appendFormat:@"获取使用3DES Key:\"%@\" 3DES CBC加密后的NSData: %lud\n", cl_encodingKey, ex_3desEncryptData.length];
+    [self.ex_textViewString appendFormat:@"获取使用3DES Key:\"%@\" 3DES ECB加密后的NSData: %lud\n", cl_encodingKey, ex_3desECBEncryptData.length];
+    
+    NSData *ex_3desDecryptData = [ex_3desEncryptData cl_decryptedDataWith3DESKey:cl_encodingKey
+                                                                     decryptData:ex_tranformImageData];
+    NSData *ex_3desECBDecryptData = [ex_3desECBEncryptData cl_decryptedECBDataWithAESKey:cl_encodingKey
+                                                                             decryptData:ex_tranformImageData];
+
+    NSLog(@"解密后的3DES CBC长度: %ld", ex_3desDecryptData.length);
+    NSLog(@"解密后的3DES ECB长度: %ld", ex_3desECBDecryptData.length);
+    
+    UIImage *ex_decryptImage = [UIImage imageWithData:ex_3desDecryptData];
+    
+    UIImageView *ex_imageView = [[UIImageView alloc] initWithImage:ex_decryptImage];
+    
+    ex_imageView.frame = self.view.bounds;
+    
+    [self.view addSubview:ex_imageView];
 }
 
 @end
