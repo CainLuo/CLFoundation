0a2c6420bbb4b3c6dcf2c586743ee98cc73b2d9f
CainLuo <350116542@qq.com>
2018-10-16 17:20:51 +0800

    添加使用示例

--- CLFoundationExample/CLFoundationExample/Module/EXBundle/EXBundleController.m
+++ CLFoundationExample/CLFoundationExample/Module/EXBundle/EXBundleController.m
@@ -21,6 +21,7 @@
     [self.ex_textViewString appendFormat:@"Version 号: %@\n", [NSBundle cl_getBundleShortVersionString]];
     [self.ex_textViewString appendFormat:@"Bundle ID: %@\n", [NSBundle cl_getBundleIdentifier]];
     [self.ex_textViewString appendFormat:@"显示的名字: %@\n", [NSBundle cl_getBundleDisplayName]];
+    [self.ex_textViewString appendFormat:@"Application的Scheme: %@\n", [NSBundle cl_getBundleScheme]];
     [self.ex_textViewString appendFormat:@"获取本地的Bundle名: %@\n", [NSBundle cl_getBundleFileWithName:@"CLFoundationExample"
                                                                                               type:@"bundle"]];
 
